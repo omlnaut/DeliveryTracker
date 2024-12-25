9dee3a9bddf1ce9466a648ef8b3652c38c1dd5d0
omlnaut <oneironaut.oml@gmail.com>
2024-12-25 21:37:10 +0000

    working telegram event grid

--- Infrastructure/Telegram.py
+++ Infrastructure/Telegram.py
@@ -1,8 +1,25 @@
 import datetime
+
+import telegram
 from function_app import app
 
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import logging
+
+
+def _load_token() -> str:
+    key_vault_url = "https://omlnaut-vaultier.vault.azure.net/"
+    secret_name = "TelegramBotToken"  # The name of your secret in Key Vault
+
+    # Create a secret client using the DefaultAzureCredential
+    credential = DefaultAzureCredential()
+    secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
+
+    token = secret_client.get_secret(secret_name).value or ""
+
+    return token
 
 
 @app.function_name(name="TestSendTelegramMessage")
@@ -10,8 +27,8 @@ import logging
 @app.event_grid_output(
     arg_name="output",
     event_name="TestSendTelegramMessage",
-    topic_endpoint_uri="EVENT_GRID_TOPIC_ENDPOINT_URI",
-    topic_key_setting="EVENT_GRID_KEY",
+    topic_endpoint_uri="SENDTELEGRAMMESSAGE_EVENT_GRID_URI",
+    topic_key_setting="SENDTELEGRAMMESSAGE_EVENT_GRID_KEY",
 )
 def TestSendTelegramMessage(
     req: func.HttpRequest, output: func.Out[func.EventGridOutputEvent]
@@ -20,7 +37,7 @@ def TestSendTelegramMessage(
     output.set(
         func.EventGridOutputEvent(
             id="test-id",
-            data={"tag1": "value1", "tag2": "value2"},
+            data={"message": "hello there testi"},
             subject="test-subject",
             event_type="test-event-1",
             event_time=datetime.datetime.now(),
@@ -32,5 +49,16 @@ def TestSendTelegramMessage(
 
 
 @app.event_grid_trigger(arg_name="azeventgrid")
-def SendTelegramMessage(azeventgrid: func.EventGridEvent):
+async def SendTelegramMessage(azeventgrid: func.EventGridEvent):
     logging.info("Python EventGrid trigger processed an event")
+    data = azeventgrid.get_json()
+    msg = data["message"]
+
+    token = _load_token()
+    chat_id = 190599017
+
+    bot = telegram.Bot(token)
+    async with bot:
+        await bot.send_message(text=msg, chat_id=chat_id)
+
+    logging.info(f"Telegram Message sent: {msg}")
