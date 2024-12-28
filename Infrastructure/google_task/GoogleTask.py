4100fb016ee95c9f09bf1853308e82e9b01e771d
omlnaut <oneironaut.oml@gmail.com>
2024-12-28 20:32:25 +0000

    task refactor

--- Infrastructure/google_task/GoogleTask.py
+++ Infrastructure/google_task/GoogleTask.py
@@ -1,11 +1,9 @@
-import datetime
 import logging
-import uuid
+from .azure_helper import task_output_binding, create_task_output_event, app
 from Infrastructure.telegram.azure_helper import (
     create_telegram_output_event,
     telegram_output_binding,
 )
-from function_app import app
 
 import azure.functions as func
 import json
@@ -23,28 +21,8 @@ def _load_credentials() -> Credentials:
     return Credentials.from_authorized_user_info(credentials_info)
 
 
-def create_task_output_binding(arg_name="taskOutput"):
-    return app.event_grid_output(
-        arg_name=arg_name,
-        event_name="create_task",
-        topic_endpoint_uri="CreateTask_EVENT_GRID_URI",
-        topic_key_setting="CreateTask_EVENT_GRID_KEY",
-    )
-
-
-def create_task_output_event(title: str, notes: str = "") -> func.EventGridOutputEvent:
-    return func.EventGridOutputEvent(
-        id=str(uuid.uuid4()),
-        data={"title": title, "notes": notes},
-        subject="create_task_event",
-        event_type="create_task_event",
-        event_time=datetime.datetime.now(),
-        data_version="1.0",
-    )
-
-
 @app.route(route="test_create_task")
-@create_task_output_binding()
+@task_output_binding()
 def test_create_task(
     req: func.HttpRequest, taskOutput: func.Out[func.EventGridOutputEvent]
 ):
