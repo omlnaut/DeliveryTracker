4716e284932c7af694ae1ece537af2dac7f0685d
omlnaut <oneironaut.oml@gmail.com>
2025-01-19 19:52:37 +0000

    poc: file upload to drive

--- google_token/create_credentials.py
+++ google_token/create_credentials.py
@@ -4,6 +4,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/tasks",
+    "https://www.googleapis.com/auth/drive.file",
 ]
 
 
