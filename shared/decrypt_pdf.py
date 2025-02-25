9548084f66c9a373dcdcbc8a9a3db160201d4972
omlnaut <oneironaut.oml@gmail.com>
2025-02-25 18:37:06 +0000

    comments

--- shared/decrypt_pdf.py
+++ shared/decrypt_pdf.py
@@ -7,6 +7,19 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 def remove_password_from_pdf(
     pdf_file: BufferedReader, password: str, output_path: Path | None = None
 ) -> Path:
+    """
+    Remove the password from a PDF file and save the decrypted PDF to a new file.
+
+    Args:
+        pdf_file (BufferedReader): A buffered reader object representing the encrypted PDF file.
+        password (str): The password to decrypt the PDF file.
+        output_path (Path | None, optional): The path where the decrypted PDF file will be saved.
+                                             If None, the decrypted PDF will be saved with the same
+                                             name as the original file, but with a ".decrypted.pdf" suffix.
+
+    Returns:
+        Path: The path to the decrypted PDF file.
+    """
     # read to lib
     pdf = PdfFileReader(pdf_file)
     pdf.decrypt(password)
